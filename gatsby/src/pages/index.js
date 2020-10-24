6168f034ba5b1df5f5b2dd8f4a15b21612a27452
Stefan Åhlander <cs.ahlander@gmail.com>
2020-10-24 19:03:38 +0200

    add data and loading images to home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,51 @@
 import React from 'react';
 
-export default function HomePage() {
+import useLatestData from '../utils/useLatestData';
+
+import { HomePageGrid } from '../styles/Grids';
+import LoadingGrid from '../components/LoadingGrid';
+import ItemGrid from '../components/ItemGrid';
+
+function CurrentlySlicing({ slicemasters }) {
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-    </>
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Slicemasters on</span>
+      </h2>
+      <p>Standing by, ready to slice you up!</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
+    </div>
+  );
+}
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Hot Slices</span>
+      </h2>
+      <p>Come on by, buy the slice!</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>No hot slices at the mo!</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
+    </div>
+  );
+}
+
+export default function HomePage() {
+  const { hotSlices, slicemasters } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The best pizza downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }
---
+++ gatsby/src/utils/useLatestData.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+
+const gql = String.raw;
+
+const deets = `
+  name
+  _id
+  image {
+    asset {
+      url
+      metadata {
+        lqip
+      }
+    }
+  }
+`;
+
+export default function useLatestData() {
+  const [hotSlices, setHotSlices] = useState();
+  const [slicemasters, setSlicemasters] = useState();
+
+  useEffect(() => {
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: gql`
+          query {
+            StoreSettings(id: "downtown") {
+              name
+              slicemaster {
+                ${deets}
+              }
+              hotslices {
+                ${deets}
+              }
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setHotSlices(res.data.StoreSettings.hotslices);
+        setSlicemasters(res.data.StoreSettings.slicemaster);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return {
+    hotSlices,
+    slicemasters,
+  };
+}
